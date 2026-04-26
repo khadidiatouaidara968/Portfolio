6dad714298f01edfdc47ba4d0e7ea0b5189540e4
khadidiatouaidara968 <khadidiatouaidara968@gmail.com>
2026-04-26 19:10:45 +0000

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.header("PROFIL")
+st.write("Etant technicienne supérieure en Géomatique,motivéé et sérieuse,je suis animée par une réelle passion pour ce domaine et je souhaite mettre mes compétences et mon savoir-faire au service de votre structure ")
+
+st.header("PARCOURS")
+st.write("Baccalauréat:d'office en 2024 UCAD")
+st.write("Brevet de Technicien Supérieur en Géomatique")
+
+st.header("FORMATION")
+st.write("licence en Géomatique au centre de Formation et d'Entreprenariat le G15(2026-2027)")
+
+st.header("COMPETENCES")
+st.write("Python")
+st.write("SIG(ArcMap,QGIS)")
+st.write("Autocad")
+st.write("Technique de Base en Géographie")
+st.write("Géoréferencement")
+
+st.header("EXPERIENCES")
+st.write("Stage en Topographie:durée de 3 mois")
+
+st.header("OUTILS")
+st.write("suite office ,OTC")
+
+st.write("A la recherche d'une premiére expérience professionelle,je suis prete à apprendre et à m'adapter rapidement inchaalah merci")
+
+with st.sidebar:
+    st.header("Khadidiatou Aidara")
+    st.write("Technicienne Supérieure en Géomatique")
+    st.markdown("""Email:khadidiatouaidara968@gmail.
+    com""")
+    st.write("Adresse:Dakar,Sénégal")
